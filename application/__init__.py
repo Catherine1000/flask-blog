fd8ccdf972d1a241545e25c958102e76f6c31c25
Catherine1000 <cathwilson00@gmail.com>
2020-03-11 12:44:17 -0400

    Login Function

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,17 @@
 from os import getenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+
 app.config['SQLALCHEMY_DATABASE_URI']=getenv('DATABASE_URI')
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 db=SQLAlchemy(app)
-from application import routes
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+from application import routes
