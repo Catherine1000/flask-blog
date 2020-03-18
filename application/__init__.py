c801f995264540b298c60e80ed13aed82e3f2559
Catherine1000 <cathwilson00@gmail.com>
2020-03-18 08:07:51 -0400

    app.config

--- application/__init__.py
+++ application/__init__.py
@@ -7,7 +7,8 @@ from flask_login import LoginManager
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-app.config['SQLALCHEMY_DATABASE_URI']=getenv('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 db=SQLAlchemy(app)
 
